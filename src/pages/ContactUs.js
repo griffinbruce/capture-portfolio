40db1c8133af1505da4206a31d63d24b0e1deafd
Griffin Schulert <griffin.bruce@gmail.com>
2020-12-29 17:01:20 -0500

    Page Transitions

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,10 +1,18 @@
-
+//Animations
+import { motion } from 'framer-motion';
+import { pageAnimation } from '../animation';
 
 const ContactUs = () => {
     return (
-        <div>
+        <motion.div
+            variants={pageAnimation}
+            initial='hidden'
+            animate='show'
+            exit='exit'
+            style={{background:'#fff'}}
+        >
             <h1>Our Contact</h1>
-        </div>
+        </motion.div>
     );
 };
 
