a17eb7ec1ab572f3fd89181da9a3e06f263961e0
Griffin Schulert <griffin.bruce@gmail.com>
2020-12-29 17:15:53 -0500

    About Section Animations

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -3,6 +3,7 @@ import home1 from '../img/home1.png';
 import { StyleAbout, StyleDescription, StyleImage, StyleHide } from '../styles';
 //Framer Motion
 import { motion } from 'framer-motion';
+import { titleAnim, fade, photoAnim } from '../animation';
 
 const AboutSection = () => {
 
@@ -11,26 +12,31 @@ const AboutSection = () => {
             <StyleDescription>
                 <motion.div>
                     <StyleHide>
-                        <motion.h2>
+                        <motion.h2 variants={titleAnim}>
                             We work to make
                         </motion.h2>
                     </StyleHide>
                     <StyleHide>
-                        <motion.h2>
+                        <motion.h2 variants={titleAnim}>
                             your <span>dreams</span>
                         </motion.h2>
                     </StyleHide>
                     <StyleHide>
-                        <motion.h2>
+                        <motion.h2 variants={titleAnim}>
                             come true.
                         </motion.h2>
                     </StyleHide>
                 </motion.div>
-                <p>Contact us for any photography or videography ideas that you have. We have professionals with amazing skills.</p>
-                <button>Contact Us</button>
+                <motion.p variants={fade}>
+                    Contact us for any photography or videography ideas that you have. We have professionals with amazing skills.
+                    </motion.p>
+                <motion.button variants={fade}>Contact Us</motion.button>
             </StyleDescription>
             <StyleImage>
-                <img src={home1} alt="guy with a camera" />
+                <motion.img
+                    variants={photoAnim}
+                    src={home1}
+                    alt="guy with a camera" />
             </StyleImage>
         </StyleAbout>
     );
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -9,7 +9,12 @@ import { pageAnimation } from '../animation';
 
 const AboutUs = () => {
     return (
-        <motion.div variants={pageAnimation} initial='hidden' animate='show' exit='exit'>
+        <motion.div
+            variants={pageAnimation}
+            initial='hidden'
+            animate='show'
+            exit='exit'
+        >
             <AboutSection />
             <ServicesSection />
             <FaqSection />
