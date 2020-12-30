31250f82354f5996bdaefa306ef07ffe5465c8b4
Griffin Schulert <griffin.bruce@gmail.com>
2020-12-30 11:23:34 -0500

    Contact and Work Animations

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -6,9 +6,12 @@ import theracer from '../img/theracer-small.png';
 import goodtimes from '../img/goodtimes-small.png';
 //Animations
 import { motion } from 'framer-motion';
-import { pageAnimation, fade, photoAnim, lineAnim, slider, sliderContainer } from '../animation';
+import { pageAnimation, fade, photoAnim, lineAnim, slider, sliderContainer, swoop } from '../animation';
+import { useScroll } from '../components/useScroll';
 
 const OurWork = () => {
+    const [element, controls] = useScroll();
+    const [element2, controls2] = useScroll();
     return (
         <Work
             variants={pageAnimation}
@@ -32,16 +35,16 @@ const OurWork = () => {
                     </Hide>
                 </Link>
             </Movie>
-            <Movie>
+            <Movie ref={element} variants={fade} animate={controls} initial='hidden'>
                 <h2>The Racer</h2>
-                <div className="line"></div>
+                <motion.div variants={lineAnim} className="line"></motion.div>
                 <Link to='/work/the-racer'>
                     <img src={theracer} alt="Woman Car Racer"/>
                 </Link>
             </Movie>
-            <Movie>
+            <Movie ref={element2} variants={fade} animate={controls2} initial='hidden'>
                 <h2>Good Times</h2>
-                <div className="line"></div>
+                <motion.div variants={lineAnim} className="line"></motion.div>
                 <Link to='/work/good-times'>
                     <Hide>
                         <img src={goodtimes} alt="Loving Couple by the Water" />
@@ -60,7 +63,7 @@ const Work = styled(motion.div)`
         padding: 1rem 0rem;
     }
 `;
-const Movie = styled.div`
+const Movie = styled(motion.div)`
     padding-bottom: 10rem;
     .line{
         height: 0.5rem;
