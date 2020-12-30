938030966d00cbe5dc4b8fe49a78d9f8dbf378b7
Griffin Schulert <griffin.bruce@gmail.com>
2020-12-30 12:51:59 -0500

    Reconfigured Services Section

--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -12,69 +12,113 @@ import { scrollReveal } from '../animation';
 import { useScroll } from './useScroll';
 
 //Array of Card Info
-const cards = [
-    { icon: clock, h1Text: 'Efficient', pText: 'So efficient!' },
-    { icon: teamwork, h1Text: 'Teamwork', pText: 'Great Teamwork' },
-    { icon: diaphragm, h1Text: 'Pro Gear', pText: 'Incredible Tools' },
-    { icon: money, h1Text: 'Money', pText: 'Money > No Money' } 
-]
+// const cards = [
+//   { icon: clock, h1Text: 'Efficient', pText: 'So efficient!',},
+//   { icon: teamwork, h1Text: 'Teamwork', pText: 'Great Teamwork',},
+//   {
+//     icon: diaphragm,
+//     h1Text: 'Pro Gear',
+//     pText: 'Incredible Tools',
+//   },
+//   { icon: money, h1Text: 'Money', pText: 'Money > No Money',},
+// ];
 
 const ServicesSection = () => {
-    const [element, controls] = useScroll();
-    return (
-        <Services variants={scrollReveal} animate={controls} initial='hidden' ref={element}>
-            <StyleDescription>
-                <h2>High <span>quality</span> services</h2>
-                <Cards>
-                    {cards.map(card => {
-                        return(
-                            <Card>
-                                <div className="icon">
-                                    <img src={card.icon} alt="icon" />
-                                    <h3>{card.h1Text}</h3>
-                                </div>
-                                <p>{card.pText}</p>
-                            </Card>
-                    )})}
-                </Cards>
-            </StyleDescription>
-            <StyleImage>
-                <img src={home2} alt="video camera"/>
-            </StyleImage>
-        </Services>
-    );
+  const [element, controls] = useScroll();
+  return (
+    <Services
+      variants={scrollReveal}
+      animate={controls}
+      initial='hidden'
+      ref={element}>
+      <StyleDescription>
+        <h2>
+          High <span>quality</span> services
+        </h2>
+        <Cards>
+          <Card>
+            <div className='icon'>
+              <img alt='icon' src={clock} />
+              <h3>Efficient</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </Card>
+          <Card>
+            <div className='icon'>
+              <img alt='icon' src={teamwork} />
+              <h3>Teamwork</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </Card>
+          <Card>
+            <div className='icon'>
+              <img alt='icon' src={diaphragm} />
+              <h3>Diaphragm</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </Card>
+          <Card>
+            <div className='icon'>
+              <img alt='icon' src={money} />
+              <h3>Affordable</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </Card>
+        </Cards>
+      </StyleDescription>
+      <StyleImage>
+        <img src={home2} alt='video camera' />
+      </StyleImage>
+    </Services>
+  );
 };
 
 const Services = styled(StyleAbout)`
-    h2{
-        padding-bottom: 5rem;
-    }
-    p{
-        width: 70%;
-        padding: 2rem 0rem 4rem 0rem;
-    }
+  h2 {
+    padding-bottom: 5rem;
+  }
+  p {
+    width: 70%;
+    padding: 2rem 0rem 4rem 0rem;
+  }
 `;
 
 const Cards = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    @media (max-width: 1300px) {
-        justify-content: center;   
-    }
+  display: flex;
+  flex-wrap: wrap;
+  @media (max-width: 1300px) {
+    justify-content: center;
+  }
 `;
 
 const Card = styled.div`
-    flex-basis: 20rem;
-    .icon{
-        display: flex;
-        align-items: center;
-        h3{
-            margin-left: 1rem;
-            background: white;
-            color: black;
-            padding: 1rem;
-        }
+  flex-basis: 20rem;
+  .icon {
+    display: flex;
+    align-items: center;
+    h3 {
+      margin-left: 1rem;
+      background: white;
+      color: black;
+      padding: 1rem;
     }
+  }
 `;
 
 export default ServicesSection;
+
+//Not working yet for unique key
+
+  /* <Cards>
+  {cards.map((card) => {
+    return (
+      <Card key={cards.id}>
+        <div className='icon'>
+          <img src={card.icon} alt='icon' />
+          <h3>{card.h1Text}</h3>
+        </div>
+        <p>{card.pText}</p>
+      </Card>
+    );
+  })}
+</Cards>; */
